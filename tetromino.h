8c5d49ec37eda810d9af9452f1a7bba0deba7dfc
user0202jo <163116738+user0202jo@users.noreply.github.com>
2024-04-21 23:48:00 +0900

    Update tetromino.h

--- tetromino.h
+++ tetromino.h
@@ -2,79 +2,7 @@
 #define TETROMINO_H
 
 #include <string>
-
-#define SHADOW_STRING "⛶"
-#define BLOCK_STRING "■"
-
-class Tetromino {
-  static const int MAX_SIZE = 4;
-
-  // 디버그용 이름
-  std::string name_;
-
-  // 테트로미노의 모양을 저장하는 배열
-  // shape[x][y]가 true인 경우 x, y 위치에 블록이 있는 것이고 false의 경우 없는
-  // 것이다.
-  bool shape_[MAX_SIZE][MAX_SIZE];
-
-  // 회전되지 않은 원래 테트로미노 객체를 저장하는 포인터
-  Tetromino *original_;
-
-  // 테트로미노의 사이즈
-  int size_;
-
-public:
-  // 사이즈와 shape 문자열을 통해 생성한다.
-  // 문자열은 size * size 길이의 문자열이 주어진다.
-
-  // 블록이 존재한다면 O를 통해 표시하고 아니라면 X를 통해 표시한다.
-  // 문자열은 개행 없이 일렬로 나열되어 주어진다.
-  // 예를 들어 T 미노의 모양과 생성은 다음과 같다.
-  // XOX
-  // OOO
-  // XXX
-  //
-  // Tetromino T("T", 3, "XOXOOOXXX")
-
-  // I 미노의 경우 다음과 같다.
-  // XXXX
-  // OOOO
-  // XXXX
-  // XXXX
-  //
-  // Tetromino I("I", 4, "XXXXOOOOXXXXXXXX")
-  Tetromino(std::string name, int size, std::string shape);
-
-  // 이름을 반환한다.
-  std::string name() { return name_; }
-
-  // 테트로미노의 사이즈를 반환한다.
-  int size() { return size_; }
-
-  // 회전되지 않은 원래 테트로미노 객체의 포인터를 반환한다.
-  Tetromino *original() { return original_; }
-
-  // 시계 방향으로 회전한 모습의 테트로미노 객체를 반환한다.
-  Tetromino rotatedCW();
-
-  // 반시계 방향으로 회전한 모습의 테트로미노 객체를 반환한다.
-  Tetromino rotatedCCW();
-
-  // 화면의 x, y 위치에 s 문자열로  테트로미노를 그린다
-  void drawAt(std::string s, int x, int y);
-
-  // 테트로미노의 좌상단 기준 x, y 위치에 블록이 있는지 여부를 나타내는 함수
-  bool check(int x, int y) { return shape_[x][y]; }
-
-  // 각 테트로미노 종류에 대한 선언
-  // cpp 파일에서 Tetromino Tetromino::I(...); 와 같이 구현한다
-  static Tetromino I, O, T, S, Z, J, L;
-
-  #ifndef TETROMINO_H
-#define TETROMINO_H
-
-#include <string>
-#include <console.h>
+#include "console.h"
 
 #define SHADOW_STRING "⛶"
 #define BLOCK_STRING "■"
@@ -151,43 +79,49 @@ public:
     }
   }
 
-  Tetromino Tetromino::rotatedCW(){
-    for (int i = 0; i < size_; i++) {
-        for (int j = i; j < size_; j++) {
-            std::swap(shape_[i][j], shape_[j][i]);
+  Tetromino Tetromino::rotatedCW() {
+    std::string CWtetromino;
+    for(int i = 0; i < size(); i++) {
+        for(int j = size()-1; j >= 0; j--) {
+          if(shape_[i][j] == true){
+            CWtetromino.append("O");
+          }
+          else{
+            CWtetromino.append("X");
+          }
         }
     }
-
-    for (int i = 0; i < size_; i++) {
-        for (int j = 0; j < size_ / 2; j++) {
-            std::swap(shape_[i][j], shape_[i][size_ - 1 - j]);
-        }
-    }
+    Tetromino result = Tetromino(name(), size(), CWtetromino);
+    result.original_ = original();
+    return result;
   }
 
-  Tetromino Tetromino::rotatedCCW(){
-    for (int i = 0; i < size_; i++) {
-        for (int j = i; j < size_; j++) {
-            std::swap(shape_[i][j], shape_[j][i]);
+
+  Tetromino Tetromino::rotatedCCW() {
+    std::string CWtetromino;
+    for(int i = size()-1; i >= 0; i--) {
+        for(int j = 0; j < size(); j++) {
+            if(shape_[i][j] == true){
+            CWtetromino.append("O");
+        }
+        else{
+            CWtetromino.append("X");
+          }
         }
     }
-
-    for(int j = 0; j < size_; j++){
-      for(int i = 0; i < size_ / 2; i++){
-        std::swap(shape_[i][j], shape_[size_ - 1 - i][j]);
-      }
-    }
+    Tetromino result = Tetromino(name(), size(), CWtetromino);
+    result.original_ = original();
+    return result;
   }
 
-  void drawAt(std::string s, int x, int y){
-    if(check(x, y) == true){
-      console::draw(x, y, s);
+  void Tetromino::drawAt(std::string s, int x, int y) {
+    for (int i = 0; i < size_; ++i) {
+        for (int j = 0; j < size_; ++j) {
+            if (check(i, j)) {
+                console::draw(x + i, y + j, s);
+            }
+        }
     }
-    
   }
 };
-
-#endif
-};
-
 #endif
